a8bfd99550a1e2bef59478178461b722a4275dc3
Waldemar Schwan <waldemar.schwan@adcloud.com>
2013-04-04 16:00:13 +0200

    added drawing library

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+  :dependencies [
+    [org.clojure/clojure "1.5.0"]
+    [quil "1.6.0"]]
+  :main clojure-intro-workshop.core)
