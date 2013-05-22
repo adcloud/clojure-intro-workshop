d29f7221d7be67eeabf1aa2801055375367ee0f9
Waldemar <waldemar.schwan@adcloud.com>
2013-05-22 22:39:33 +0200

    removed GUI and any need for threads and agents

--- src/clojure_intro_workshop/core.clj
+++ src/clojure_intro_workshop/core.clj
@@ -1,5 +1,5 @@
-(ns clojure-intro-workshop.core
-  (:use quil.core)) ;; quil is our drawing library
+(ns clojure-intro-workshop.core)
+
 
 (defn create-world
   "Return a vector of vectors, which represents a 2D field with the
@@ -9,11 +9,14 @@
     (for [_ (range width)]
       (rand-int 2))))
 
+
 (defn world-width [world]
   (count (nth world 0)))
 
+
 (defn world-height [world]
   (count world))
+
 
 (defn element-at [world [x y]]
   (if (or (< x 0)
@@ -23,67 +26,38 @@
     0 ;; respect array bounds
     (nth (nth world y) x)))
 
+
 (defn pretty-print-world
   "Handy debugging util."
   [world]
   (doseq [row world]
     (println row)))
 
-;; setting up state
-;; --------------------------
-(def current-world (agent (create-world 50 40)))
+(def ppw pretty-print-world) ; alias for easy access in repl
 
+
+(defn live
+  "This function will apply the givne update-fn cycles-times,
+  print the steps and return the result"
+  ([world update-fn]
+    (live world update-fn 2)) ; use a default value of 2
+
+  ([world update-fn cycles]
+   (if (zero? cycles)
+     ;; true case - just return the world
+     world
+     ;; false case - update, print and go again
+     (let [new-world (update-fn world)] ; build new world
+       (println "-------------------------")
+       (pretty-print-world new-world) ; print it
+       (recur new-world update-fn (dec cycles)))))) ; run again until cycles are zero
+
+;; ------------------
+;; Get going there
+;; ..................
 (defn update-world [world]
   ;; This is your chance to change the world! ;)
   world)  ;; return new world
 
-(def tick-delay 2000)
-
-(defn tick [world]
-    ;; *agent* is dynamically bound to the agent this function was sent to
-    (send *agent* update-world)  ;; queue an update
-    (send *agent* tick)  ;; queue the next tick
-    (Thread/sleep tick-delay)  ;; delay the queue for a while
-    world)  ;; always return a world which becomes the agent's new state
-
-(defn start-main-loop []
-  (send current-world tick))
-
-;; rendering functions
-;; --------------------------
-
-;; called only once
-(defn setup []
-  (smooth)
-  (frame-rate 20)
-  (background 0)) ;; black background
-
-;; called for each frame
-(defn draw []
-  (stroke 0)
-  (stroke-weight 0)
-  (let [tile-width (/ (width) (world-width @current-world))
-        tile-height (/ (height) (world-height @current-world))]
-    (dorun  ;; force immediate evaluation of lazy sequence
-      (for [x (range (world-width @current-world))
-            y (range (world-height @current-world))]
-        (do  ;; we do our drawing side-effects here
-          ;; set fill based on cell alive state
-          (fill (* 255 (element-at @current-world [x y])))
-          ;; draw a rect for each cell
-          (rect (* x tile-width) (* y tile-height)
-                tile-width tile-height))))))
-
-
-;; main function
-;; --------------------------
-(defn -main []
-  ;; this is quil starting a drawing cycle
-  (defsketch example
-    :title "Game of Life"
-    :setup setup
-    :draw draw
-    :size [(* (world-width @current-world) 15),
-           (* (world-height @current-world) 15)])
-  ;; start the never-ending main loop
-  (start-main-loop))
+;; create new world and do 2 cycles
+(live (create-world 5 5) update-world 2)
