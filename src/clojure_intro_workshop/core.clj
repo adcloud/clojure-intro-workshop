c903c1cb468d5f8d659df7577a3d4bdd8285c9f1
Waldemar <waldemar.schwan@adcloud.com>
2013-06-15 15:21:58 +0200

    switch to a optional improved GUI done.

--- src/clojure_intro_workshop/core.clj
+++ src/clojure_intro_workshop/core.clj
@@ -1,5 +1,6 @@
 (ns clojure-intro-workshop.core
-  (:use quil.core))
+  (:use quil.core ; GUI rendering library
+        [clojure.stacktrace :only [print-stack-trace]]))
 
 
 (defn create-world
@@ -11,7 +12,9 @@
       (rand-int 2))))
 
 
-(defn create-blinker[]
+(defn create-blinker
+  "Creates a tiny world that containa blinker."
+  [] ; empty argument vector
   [[0 1 0]
    [0 1 0]
    [0 1 0]])
@@ -38,7 +41,11 @@
   (count world))
 
 
-(defn element-at [world [x y]]
+(defn element-at
+  "Returns the element at the given position.
+  If the position is outside the array it will return
+  0, marking dead space."
+  [world [x y]]
   (if (or (< x 0)
           (< y 0)
           (>= x (world-width world))
@@ -56,9 +63,8 @@
 
 (def ppw pretty-print-world) ; alias for easy access in repl
 
-
 (defn live
-  "This function will apply the givne update-fn cycles-times,
+  "This function will apply the given update-fn cycles-times,
   print the steps and return the result"
   [world update-fn cycles]
    (if (zero? cycles)
@@ -68,69 +74,146 @@
      (let [new-world (update-fn world)] ; build new world
        (println)
        (pretty-print-world new-world) ; print it
-       (update-world new-world) ;; display each step of the world
        (recur new-world update-fn (dec cycles))))) ; run again until cycles are zero
 
-;; ------------------
+
+
 ;; Get going there
-;; ..................
+; -----------------------------------------------------------------------------
 (defn update-world [world]
   ;; This is your chance to change the world! ;)
-  world)  ;; return new world
+  world
+  )  ;; return new world
 
-;; create new world and do 2 cycles
-; (live (create-blinker) update-world 2)
+; To start the GUI call:
+; (start-rendering)
+;
+; To dispay a world call:
+; (display-world (create-blinker))
+;
+; To start a simulation of a starting world call:
+; (start-simulation (create-blinker))
+;
+; To stop the simulation:
+; (stop-simulation)
+;
+; In case of an exception adjust you update-world function and restart the
+; simulation (no need to restart anything else):
+; (start-simulation (create-blinker))
+;
+; If you prevere simple text the function ppw and live are your friends :) .
+;
+; Good luck! Have Fun. :)
+;
+;
+; PS:
+;
+; Adjust the simulation speed by calling:
+; (reset! simulation-sleep-ms 1000)
 
-(def w (create-blinker))
-
-;; this is a no go in normal development but for a repl it will do
-;; by running this again and again you can see the change over time
-(def w (live w update-world 1))
 
 
 
-;; #####################################################
-;; here be dragons
 
+
+
+
+
+
+
+
+; -----------------------------------------------------------------------------
+; here be dragons
+;
+; Code in here is stuff neede for the fancy GUI repensentation.
+; It deals with threads and shares variables. While this is fun and fairly easy
+; to write and coordinate in clojure it still should be considered a 2nd step.
+; Feel free to brows this code, but you don't have to understand any of this
+; right now ;).
+; -----------------------------------------------------------------------------
+
+; The shared global state of a world.
 (def rendered-w (atom [[1 0]
                        [0 1]]))
 
-(def update-world [w]
-  (reset! rendered-w))
+; this serves as the thread that runs the endless simulation loop
+(def simulation-running (agent false))
+(def simulation-sleep-ms (atom 400))
+(def fps (atom 50))
+
+(defn simulate
+  "Executes the side effect of changing the rendered-w to the next state
+  calculated by update-world.
+  Also resends itself to the agent to create a endless loop.
+  Can be stoped by setting simulation-running to false.
+  Use stop-simulation for this."
+  [running]
+  (when running
+    (try
+      (swap! rendered-w update-world) ; side effect! assign new state
+      (Thread/sleep @simulation-sleep-ms)
+      (send *agent* simulate) ; trigger the loop
+      true ; this is the new value for simulation-running
+      (catch Exception e
+        (println "Caught exception during simulation press <enter> to proceed.")
+        (print-stack-trace e)
+        (println "To restart the simulation simply call
+                 (start-simulation <world_value>) again.")
+        false)))) ; in case of an exception stop the loop
+
+(defn start-simulation
+  "Starts to simulate the world in a seperate thread.
+  Displaying the result in the GUI.
+  Call stop-simulation to stop that."
+  [w]
+  (send simulation-running (constantly true))
+  (send simulation-running simulate))
+
+(defn stop-simulation
+  "Stops the simulation loop."
+  []
+  (send simulation-running (constantly false)))
+
+(defn display-world
+  "Displays the given world in the GUI.
+  Also stops the simulation."
+  [w]
+  (stop-simulation) ; side effect
+  (reset! rendered-w w))
+
 
 ;; called only once
+; -----------------------------------------------------------------------------
 (defn setup []
   (smooth)
-  (frame-rate 20)
+  (frame-rate @fps)
   (background 0)) ;; black background
 
 ;; called for each frame
+; -----------------------------------------------------------------------------
 (defn draw []
   (stroke 0)
   (stroke-weight 0)
-  (let [tile-width (/ (width) (world-width rendered-w))
-        tile-height (/ (height) (world-height rendered-w))]
+  (let [tile-width (/ (width) (world-width @rendered-w))
+        tile-height (/ (height) (world-height @rendered-w))]
     (dorun  ;; force immediate evaluation of lazy sequence
-      (for [x (range (world-width rendered-w))
-            y (range (world-height rendered-w))]
+      (for [x (range (world-width @rendered-w))
+            y (range (world-height @rendered-w))]
         (do  ;; we do our drawing side-effects here
           ;; set fill based on cell alive state
-          (fill (* 255 (element-at rendered-w [x y])))
+          (fill (* 255 (element-at @rendered-w [x y])))
           ;; draw a rect for each cell
           (rect (* x tile-width) (* y tile-height)
                 tile-width tile-height))))))
 
 
 ;; main function
-;; --------------------------
+; -----------------------------------------------------------------------------
 (defn start-rendering []
   ;; this is quil starting a drawing cycle
   (defsketch example
     :title "Game of Life"
     :setup setup
     :draw draw
-    :size [(* (world-width rendered-w) 15),
-           (* (world-height rendered-w) 15)]))
-
-(def w (create-world 3 7))
-(start-rendering)
+    :size [(* (world-width @rendered-w) 15),
+           (* (world-height @rendered-w) 15)]))
